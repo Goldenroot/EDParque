6560f63909bf42f7652cb37eb3bce28e759d5edb
Goldenroot <miguelferreira142001@gmail.com>
2021-10-09 20:56:59 +0100

    First Commit

---
+++ app/Models/Profile.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Profile extends Model
+{
+    use HasFactory;
+
+    public $fillable = ['user_id', 'banner', 'about', 'location'];
+
+    public function user(){
+
+        return $this->belongsTo('App\Models\User');
+    }
+}
